99a82b38f081351117660c042bfed59de21fcca5
cahyadie <cahyadiilham12@gmail.com>
2024-12-29 10:14:03 +0700

    codingan di kelas

---
+++ app/src/main/java/com/example/prak10/model/Mahasiswa.kt
@@ -0,0 +1,17 @@
+package com.example.prak10.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Mahasiswa(
+    val nim: String,
+    val nama: String,
+    val alamat: String,
+
+    @SerialName("jenis_kelamin")
+    val jeniskelamin: String,
+    val kelas: String,
+    val angkatan: String
+
+)
